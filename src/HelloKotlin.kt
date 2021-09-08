c7c52f9189271615c235f3f2d8495a3bc7b755b6
sobamemil <sieh96@naver.com>
2021-09-08 11:46:37 +0900

    if else 문의 간략화, 변수 선인 시 if else 문 사용

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -56,15 +56,30 @@ fun main() {
 //
 //    println("res1: $res1, res2: $res2")
 
-    // 아래 두개 동일 코드
-    var max: Int
-    if (a > b)
-        max = a
-    else
-        max = b
+//    // 아래 두개 동일 코드
+//    var max: Int
+//    if (a > b)
+//        max = a
+//    else
+//        max = b
+//
+//    // -->
+//    val max = if (a > b) a else b
 
-    // -->
-    val max = if (a > b) a else b
+    val a = 12
+    val b = 7
+
+    // 블록과 함께 사용
+    val max = if(a > b) {
+        println("a 선택")
+        a // 마지막 식인 a가 반환되어 max에 할당
+    }
+    else {
+        println("b 선택")
+        b // 마지막 식인 b가 반환되어 max에 할당
+    }
+
+    println(max)
 }
 
 //fun highFunc(sum: (Int, Int) -> Int, a: Int, b: Int): Int = sum(a, b) // sum 매개변수는 함수
